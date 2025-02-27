c5ad6db83f9f8f365347e036aaa79e9c36b8d7d0
Parsa Pournami <135994690+Parsa-developer@users.noreply.github.com>
2025-02-27 15:42:16 +0330

    Add files via upload

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.email
